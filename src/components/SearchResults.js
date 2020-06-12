1c6155fa6e044e72445a430afab43ccdd18667fa
Kevin Hassenkamp <khassenkamp@gmail.com>
2020-06-11 23:12:45 -0500

    deckbuilder day 02

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import './SearchResults.css';
+
+import Card from './Card';
+
 const SearchResults = ({ results }) => {
   return (
     <div id="results">
         <h3>Here's what we found ({ results.length } results):</h3>
+        <div className='CardList'>
+            {
+                results.map(result => (
+                    <Card key={result.id} {...result} />
+                ))
+            }
+        </div>
     </div>
   );
 }
